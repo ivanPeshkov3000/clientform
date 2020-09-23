69e81073b9a19f7e15cf0f6d52c22f64af306a45
Vitaliy V <vitalik.voronov1989@ya.ru>
2020-09-23 23:01:58 +0300

    i'm tired

--- src/components/InputValid.vue
+++ src/components/InputValid.vue
@@ -1,14 +1,27 @@
-/* eslint-disable vue/no-side-effects-in-computed-properties */
 <template lang="pug">
     .input-validate
         label(:for="item.name") {{item.title}}
         input(
             :id="item.name"
+            v-if="item.type=='text'||'checkbox'||'radio'"
             v-model.trim="$v[item.name].$model"
             :type="item.type"
+            @input="sendData"
         )
+        select(
+            :id="item.name"
+            v-else-if="item.type=='select'||'multiselect'"
+            v-model.trim="$v[item.name].$model"
+            :type="item.type"
+            :multiple="item.type=='multiselect'"
+            @input="sendData"
+        )
+            option(
+                v-for="opt of item.options"
+            ) {{opt}}
         div.notice-valid(
             v-show="$v.$error"
+            tabindex="0"
         )
             span.notice-validator(
                 v-for="notice in noticeValidator" :class="{'invalid': notice.invalid}"
@@ -56,6 +69,12 @@ export default {
             return obj
         }
     },
+    methods: {
+        sendData: function() {
+            let value = [[this.item.name], this.$v[this.item.name]]
+            this.item.listener(value)
+        }
+    },
     validations() {
         const valid = this.item.validators
         const obj = {}
@@ -74,12 +93,36 @@ export default {
         grid-template-columns: 2fr 3fr 1fr;
         align-items: center;
         grid-gap: .5em;
+        position: relative;
+        input[type="text"] {
+            height: 30px;
+            border: 0;
+            background: none;
+            font-size: 14px;
+            border-bottom: 2px solid rgba($color: #000000, $alpha: .3);
+            outline: none;
+            &:focus {
+            border-bottom-color: rgba($color: #000000, $alpha: .6);
+            }
+        }
         .notice-valid {
+            &::before {
+                content: "\26A0";
+                font-size: 40px;
+                color: red;
+            }
+            * {
+                visibility: hidden;
+            }
             position: absolute;
-            min-width: max-content;
+            right: -120px;
             text-align: left;
+            z-index: -1;
+            
             .notice-validator {
                 display: list-item;
+                margin-left: 15px;
+                margin-right: 5px;
                 font-size: 12px;
                 list-style-type: "‎\2714  ";
                 &.invalid {
@@ -88,8 +131,17 @@ export default {
                 }
             }
         }
-        &:focus .notice-valid {
-            opacity: 1;
+        input:focus~.notice-valid {
+            min-width: max-content;
+            background-color: #fff;
+            border: 1px solid grey;
+            border-radius: 3px;
+            padding: 2px;
+            padding-left: 10px;
+            z-index: 5;
+            * {
+                visibility: visible;
+            }
         }
     }
 </style>
--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -9,199 +9,23 @@
       .bio-wrapper
         h3 Основное:
         InputValid(
-          :item="inputs.surname"
+          v-for="input in inputs.bio"
+          :item="input"
+          :key="input.name"
         )
-
-        label( for="surname") Фамилия*: 
-        input(
-          id="surname"
-          v-model.trim="$v.bio.surname.$model"
-          type="text"
-          name="surname"
-        )
-        div.notice
-          span.notice-error(v-if="$v.bio.surname.$error && !$v.bio.surname.required") Обязательное поле
-          span.notice-error(v-if="$v.bio.surname.$error && !$v.bio.surname.minLength") Не менее {{$v.bio.surname.$params.minLength.min}} символов
-        label( for="name") Имя*: 
-        input(
-          id="name"
-          v-model.trim="$v.bio.name.$model"
-          type="text"
-          name="name"
-        )
-        div.notice
-          span.notice-error(v-if="$v.bio.name.$error && !$v.bio.name.required") Обязательное поле
-          span.notice-error(v-if="$v.bio.name.$error && !$v.bio.name.minLength") Не менее {{$v.bio.name.$params.minLength.min}} символов
-        label( for="patronym") Отчество: 
-        input(
-          id="patronym"
-          v-model="$v.bio.patronym.$model"
-          type="text"
-          name="patronym"
-        )
-        div.notice
-          span.notice-error(v-if="$v.bio.patronym.$error && !$v.bio.patronym.minLength") Не менее {{$v.bio.patronym.$params.minLength.min}} символов
-        label( for="dob") Дата рождения*: 
-        input(
-          id="dob"
-          v-model.trim="$v.bio.dob.$model"
-          type="text"
-          name="dob"
-        )
-        div.notice
-          span.notice-error(v-if="$v.bio.dob.$error && !$v.bio.dob.numeric") Только цифры!
-          span.notice-error(v-if="$v.bio.dob.$error && !$v.bio.dob.required") Обязательное поле
-        label( for="phone") Номер телефона*: 
-        input(
-          id="phone"
-          v-model.trim="$v.bio.phone.$model"
-          type="text"
-          name="phone"
-        )
-        div.notice
-          span.notice-error(v-if="$v.bio.phone.$error && !$v.bio.phone.required") Обязательное поле
-          span.notice-error(v-if="$v.bio.phone.$error && !$v.bio.phone.numeric") Только цифры!
-          span.notice-error(v-if="$v.bio.phone.$error && $v.bio.phone.numeric && !$v.bio.phone.minLength") Не менее {{$v.bio.phone.$params.minLength.min}} символов
-        span Пол:
-        .gender-radio
-          input(
-            id="gendermale"
-            v-model.trim="$v.bio.gender.$model"
-            type="radio"
-            name="gender"
-            value="male"
-          )
-          label(for="gendermale") муж.
-          input(
-            id="genderfemale"
-            v-model.trim="$v.bio.gender.$model"
-            type="radio"
-            name="gender"
-            value="female"
-          )
-          label(for="genderfemale") жен.
-        div.notice
-        label(
-          for="group"
-          :class="{ 'invalid': $v.bio.group.$error }"
-          ) Группа клиентов*:
-        select(
-          id="group"
-          v-model.trim="$v.bio.group.$model"
-          type="text"
-          name="group"
-          multiple="on"
-        )
-          option VIP
-          option Проблемные
-          option ОМС
-        div.notice
-          span.notice-error(v-if="$v.bio.group.$error && !$v.bio.group.required") Обязательное поле
-
-        label( for="doctor") Лечащий врач: 
-        select(
-          id="doctor"
-          v-model.trim="$v.bio.doctor.$model"
-          type="text"
-          name="doctor"
-        )
-          option Иванов
-          option Захаров
-          option Чернышева
-        div.notice
-
-        label(for="sendsms") Не отправлять СМС: 
-        input(
-          id="sendsms"
-          v-model.trim="$v.bio.sendsms.$model"
-          type="checkbox"
-          name="sendsms"
-        )
-        div.notice
       .address-wrapper
         h3 Адрес:
-        label(for="addrIndex") Индекс: 
-        input(
-          id="addrIndex"
-          v-model="address.index"
-          type="text"
-          name="addrIndex"
-          size="10"
-        )
-        label(for="country") Страна: 
-        input(
-          id="country"
-          v-model="address.country"
-          type="text"
-          name="country"
-        )
-        label(for="region") Область: 
-        input(
-          id="region"
-          v-model="address.region"
-          type="text"
-          name="region"
-        )
-        label(for="city") Город*: 
-        input(
-          id="city"
-          v-model="$v.address.city.$model"
-          type="text"
-          name="city"
-        )
-        label( for="street") Улица: 
-        input(
-          id="street"
-          v-model="address.street"
-          type="text"
-          name="street"
-        )
-        label( for="house") Дом: 
-        input(
-          id="house"
-          v-model="address.house"
-          type="text"
-          name="house"
+        InputValid(
+          v-for="input in inputs.address"
+          :item="input"
+          :key="input.name"
         )
       .passport-wrapper
         h3 Паспорт:
-        label( for="typeDocument") Тип документа*: 
-        select(
-          id="typeDocument"
-          v-model="passport.type"
-          type="text"
-          name="typeDocument"
-        )
-          option Паспорт
-          option Свидетельство о рождении
-          option Вод. удостоверение
-        label( for="docSerial") Серия: 
-        input(
-          id="docSerial"
-          v-model="$v.passport.serial.$model"
-          type="text"
-          name="docSerial"
-        )
-        label( for="docNumber") Номер: 
-        input(
-          id="docNumber"
-          v-model="passport.number"
-          type="text"
-          name="docNumber"
-        )
-        label( for="issueBy") Кем выдан: 
-        input(
-          id="issueBy"
-          v-model="passport.issueBy"
-          type="text"
-          name="issueBy"
-        )
-        label( for="issueDate") Дата выдачи*: 
-        input(
-          id="issueDate"
-          v-model="$v.passport.issueDate.$model"
-          type="text"
-          name="issueDate"
+        InputValid(
+          v-for="input in inputs.passport"
+          :item="input"
+          :key="input.name"
         )
       input.submit-create(type="submit" value="Сохранить")
 </template>
@@ -216,52 +40,209 @@ export default {
   },
   data() {
     return {
+      childata: {},
       inputs: {
-        surname: {
-          type: "text",
-          name: "surname",
-          title: "Фамилия*:",
-          validators: {
-            required: true,
-            minLength: 2,
-            alpha: true
+        bio: {
+          surname: {
+            type: "text",
+            name: "surname",
+            title: "Фамилия*:",
+            listener: this.getChildData,
+            validators: {
+              required: true,
+              minLength: 2,
+              alpha: true
+            }
+          },
+          name: {
+            type: "text",
+            name: "name",
+            title: "Имя*:",
+            listener: this.getChildData,
+            validators: {
+              required: true,
+              minLength: 2,
+              alpha: true
+            }
+          },
+          patronym: {
+            type: "text",
+            name: "patronym",
+            title: "Отчество:",
+            listener: this.getChildData,
+            validators: {
+              minLength: 2,
+              alpha: true
+            }
+          },
+          dob: {
+            type: "text",
+            name: "dob",
+            title: "Дата рождения*:",
+            listener: this.getChildData,
+            validators: {
+              required: true,
+              numeric: true
+            }
+          },
+          gender: {
+            type: "radio",
+            name: "gender",
+            title: "Пол:",
+            listener: this.getChildData
+          },
+          phone: {
+            type: "text",
+            name: "phone",
+            title: "Имя*:",
+            listener: this.getChildData,
+            validators: {
+              required: true,
+              minLength: 11,
+              numeric: true
+            }
+          },
+          group: {
+            type: "multiselect",
+            name: "group",
+            title: "Группа клиентов*:",
+            options: [
+              "VIP",
+              "Проблемные",
+              "ОМС"
+            ],
+            listener: this.getChildData,
+            validators: {
+              required: true
+            }
+          },
+          doctor: {
+            type: "select",
+            name: "doctor",
+            title: "Лечащий врач*:",
+            options: [
+              "Иванов",
+              "Захаров",
+              "Чернышева"
+            ],
+            listener: this.getChildData
+          },
+          sendsms: {
+            type: "checkbox",
+            name: "sendsms",
+            title: "Не отправлять смс:",
+            listener: this.getChildData
+          }
+        },
+        address: {
+          index: {
+            type: "text",
+            name: "index",
+            title: "Индекс:",
+            listener: this.getChildData,
+            validators: {
+              numeric: true
+            }
+          },
+          country: {
+            type: "text",
+            name: "country",
+            title: "Страна:",
+            listener: this.getChildData,
+            validators: {
+              alpha: true
+            }
+          },
+          region: {
+            type: "text",
+            name: "region",
+            title: "Область:",
+            listener: this.getChildData,
+            validators: {
+              alpha: true
+            }
+          },
+          city: {
+            type: "text",
+            name: "city",
+            title: "Город*:",
+            listener: this.getChildData,
+            validators: {
+              required: true,
+              alpha: true
+            }
+          },
+          street: {
+            type: "text",
+            name: "street",
+            title: "Улица:",
+            listener: this.getChildData,
+            validators: {
+              alphaNum: true
+            }
+          },
+          house: {
+            type: "text",
+            name: "house",
+            title: "Дом:",
+            listener: this.getChildData
+          }
+        },
+        passport: {
+          type: {
+            type: "text",
+            name: "type",
+            title: "Тип документа:",
+            listener: this.getChildData,
+            validators: {
+              required: true
+            }
+          },
+          serial: {
+            type: "text",
+            name: "serial",
+            title: "Серия:",
+            listener: this.getChildData
+          },
+          number: {
+            type: "text",
+            name: "number",
+            title: "Номер:",
+            listener: this.getChildData,
+            validators: {
+              numeric: true
+            }
+          },
+          issueBy: {
+            type: "text",
+            name: "issueBy",
+            title: "Кем выдан:",
+            listener: this.getChildData,
+            validators: {
+              numeric: true
+            }
+          },
+          issueDate: {
+            type: "text",
+            name: "issueDate",
+            title: "Дата выдачи:",
+            listener: this.getChildData,
+            validators: {
+              required: true
+            }
           }
         }
-      },
-      bio: {
-        name: "",
-        surname: "",
-        patronym: "",
-        dob: "",
-        phone: "7",
-        gender: "",
-        group: [],
-        doctor: [],
-        sendsms: false,
-      },
-      address: {
-        index: "",
-        country: "",
-        region: "",
-        city: "",
-        street: "",
-        house: ""
-      },
-      passport: {
-        type: [],
-        serial: "",
-        number: "",
-        issueBy: "",
-        issueDate: "",
-
       },
       form: false,
       submitErr: false
     }
   },
   methods: {
+    getChildData (childata) {
+      this.childata[childata[0]] = childata[1]
+    },
     createUser: function() {
-      console.log('Hey!')
+      console.log('Vuelidators: ', vuelidate)
       this.$v.$touch()
       if (this.$v.$invalid) {
         this.submitErr = true
@@ -270,62 +251,14 @@ export default {
       }
     }
   },
-  validations: {
-    bio: {
-      name: {
-        required: vuelidate.required,
-        minLength: vuelidate.minLength(2)
-      },
-      surname: {
-        required: vuelidate.required,
-        minLength: vuelidate.minLength(2)
-      },
-      patronym: {
-        minLength: vuelidate.minLength(2)
-      },
-      dob: {
-        required: vuelidate.required,
-        numeric: vuelidate.numeric
-      },
-      gender: {},
-      phone: {
-        required: vuelidate.required,
-        minLength: vuelidate.minLength(11),
-        numeric: vuelidate.numeric
-      },
-      group: {
-        required: vuelidate.required
-      },
-      doctor: {},
-      sendsms: {}
-    },
-    address: {
-      index: {
-        numeric: vuelidate.numeric
-      },
-      country: {},
-      region: {},
-      city: {
-        required: vuelidate.required
-      },
-      street: {},
-      house: {}
-    },
-    passport: {
-      type: {
-        required: vuelidate.required
-      },
-      serial: {},
-      number: {
-        numeric: vuelidate.numeric
-      },
-      issueBy: {},
-      issueDate: {
-        required: vuelidate.required
-      },
-
-    },
-    form: ['bio', 'address', 'passport'],
+  validations() {
+    const arr = []
+    for (let child in this.childata) {
+      arr.push(child)
+    }
+    return {
+      form: arr
+    }
   }
 }
 </script>
@@ -349,17 +282,10 @@ export default {
       border-bottom-color: rgba($color: #000000, $alpha: .6);
     }
   }
-  .notice-error {
-    padding: 0;
-    margin: 0;
-    margin-left: 10px;
-    color: red;
-    font-size: 12px;
-  }
 }
 .grid-wrapper {
   display: grid;
-  grid-template-columns: repeat(12, minmax(82px, 1fr));
+  grid-template-columns: repeat(2, 1fr);
   grid-gap: 1em;
   align-items: baseline;
   .form-title {
@@ -370,53 +296,19 @@ export default {
     justify-self: center;
   }
   .bio-wrapper {
-    display: grid;
-    grid-area: 2/1/4/7;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 1em;
-    align-items: end;
-    h3 {
-      grid-column: 1/-1;
-      justify-self: start;
-    }
-    label {
-      justify-self: start;
-      margin-left: 1em;
-      &[for="group"] {
-        align-self: baseline;
-      }
-    }
-    input {
-      justify-self: stretch;
-      &#sendsms {
-        justify-self: start;
-      }
-    }
-
-    // #group {
-    //   grid-row: span 2
-    // }
-    .notice {
-      justify-self: start;
-    }
+    display: flex;
+    flex-direction: column;
+    grid-column: 1/2;
   }
   .passport-wrapper {
-    display: grid;
-    grid-column: 7/-1;
-    grid-template-columns: repeat(2, 1fr);
-    h3 {
-      grid-column: 1/-1;
-      justify-self: start;
-    }
+    display: flex;
+    flex-direction: column;
+    grid-column: 2/-1;
   }
   .address-wrapper {
-    display: grid;
-    grid-column: 7/-1;
-    grid-template-columns: repeat(2, 1fr);
-    h3 {
-      grid-column: 1/-1;
-      justify-self: start;
-    }
+    display: flex;
+    flex-direction: column;
+    grid-column: 2/-1;
   }
 }
 
